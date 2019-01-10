df9f06061d01b7b4e236f2a5b0988d223ab85319
Angelique Montuwy <angelique.montuwy@etudiant.univ-rennes1.fr>
2019-01-10 22:13:14 +0100

    canvas + button

---
+++ Assets/Script/GameLauncher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLauncher : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    [SerializeField] GameObject playerutils;
+    private void Awake()
+    {
+       
+    }
+
+    public void PlayGame()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.PlayRace();
+            panel.SetActive(false);
+            playerutils.SetActive(true);
+
+        }
+    }
+
+    public void ReplayRace()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.RePlay();
+            panel.SetActive(false);
+        }
+    }
+
+    public void GhostRace()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.GhostRace();
+            panel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;             //This script, like MouseLocation, has a public static reference to itself to that other scripts
+                                                    //can access it from anywhere without needing to find a reference to it
+    bool gamemodePlay;
+    bool gamemodeReplay;
+    bool gamemodeGhostRace;
+    [HideInInspector]
+    public int score = 0;                                  //The player's current score
+
+    void Awake()
+    {
+        //This is a common approach to handling a class with a reference to itself.
+        //If instance variable doesn't exist, assign this object to it
+        if (Instance == null)
+            Instance = this;
+        //Otherwise, if the instance variable does exist, but it isn't this object, destroy this object.
+        //This is useful so that we cannot have more than one GameManager object in a scene at a time.
+        else if (Instance != this)
+            Destroy(this);
+    }
+
+    public void PlayRace()
+    {
+        gamemodePlay = true;
+        gamemodeGhostRace = false;
+        gamemodeReplay = false;
+    }
+
+    public void RePlay()
+    {
+        gamemodeReplay = true;
+        gamemodePlay = false;
+        gamemodeGhostRace = false;
+    }
+
+    public void GhostRace()
+    {
+        gamemodeGhostRace = true;
+        gamemodePlay = false;
+        gamemodeReplay = false;
+    }
+
+}
