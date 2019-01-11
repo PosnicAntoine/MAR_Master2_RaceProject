7aa25a0657a3cc8989f6a58dd48968311cc2472e
aphivu <alexandre.phi.vu@gmail.com>
2019-01-11 12:17:16 +0100

    add race et db management

---
+++ Assets/Script/ManageDatabase.cs
@@ -0,0 +1,73 @@
+﻿using Mono.Data.Sqlite; 
+using System.Data; 
+using System;
+using UnityEngine;
+
+public class ManageDatabase
+{
+
+    static string uri = "URI=file:" + Application.dataPath + "/Resources/Database/mar_database.db";
+    IDbConnection dbconn;  
+
+    void Connection(){
+        if (dbconn == null) {
+            dbconn = (IDbConnection) new SqliteConnection(uri);
+            dbconn.Open();
+        }
+    }
+
+
+    void Close(){
+        if (dbconn != null){
+            dbconn.Close();
+            dbconn = null;
+        }
+    }
+
+    public void getRaceTest(){
+        //Race race = new Race();
+
+        Connection();
+
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT * FROM RACE";
+
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            long id = long.Parse(reader.GetValue(0).ToString());
+            string name = reader.GetString(1);
+            float time = reader.GetFloat(2);
+            float x = reader.GetFloat(3);
+            float y = reader.GetFloat(4);
+            float z = reader.GetFloat(5);
+        
+            Debug.Log("id: "+ id + " name: " + name + " time: " + time + " x: " + x + " y: " + y + " z: " + z);
+        }
+
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+
+        Close();
+    }
+
+    public void addRace(Race race){
+        Connection();
+
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string insert ="";
+        foreach(Vector4 position in race.Trajectory){
+            insert += "INSERT INTO RACE VALUES (\'" + race.Id + "\',\'" + race.Name + "\',\'" + 
+                position.x + "\',\'" + position.y + "\',\'" + position.z + "\',\'" + position.w +"\');";
+        }
+        dbcmd.CommandText = insert;
+        dbcmd.ExecuteNonQuery();
+        Close();
+    }
+
+
+}
---
+++ Assets/Script/Race.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+Table RACE in sqlite database:
+[id,name,time,x,y,z]
+with:
+- id: numeric
+- name: text
+- time: numeric
+- x: numeric
+- y: numeric
+- z: numeric
+ */
+public class Race 
+{
+    public long Id { get; set; }
+    public string Name {get; set; }
+
+    public List<Vector4> Trajectory { get; set; } // Vector4 :(time,coord x, coord y, coord z)
+
+    public Race(){
+        Trajectory = new List<Vector4>();
+    }
+
+}
