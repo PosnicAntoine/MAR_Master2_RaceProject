ab1695b528e96292c88f850dfcc6c56385938f56
Posnic Antoine <32549290+PosnicAntoine@users.noreply.github.com>
2019-01-24 03:46:13 +0100

    Added helicopter and beginning of props. Tweaked a bit cameras aswell.

---
+++ Assets/Script/Misc/Helicopter/BackRotorBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackRotorBehavior : MonoBehaviour
+{
+
+    public float speed;
+    public bool isSpinning;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isSpinning)
+        {
+            Transform t = GetComponent<Transform>();
+            t.Rotate(speed * Time.deltaTime,0,0);
+        }
+
+    }
+
+
+}
